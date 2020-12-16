82fb4269c5ea04ccd9a3e8e1769fb15fb07f3fb4
felipe <felipep1109@gmail.com>
2020-12-15 21:58:07 -0300

    Limpieza de img y archivos desechados para diseño

--- index.php
+++ index.php
@@ -1,50 +1,56 @@
+
 <!DOCTYPE html>
-<html lang="en">
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Login admiv</title>
+    <link rel="stylesheet" href="css/style_form.css">
+  </head>
+  <body>
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Beauty Eyes</title>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
-    <link rel="stylesheet" href="css/style.css">
+    <div class="opa">
+    <div class="login-box">
+      <img src="img/45.png" class="avatar" alt="Avatar Image">
+      <h1>Login Optica</h1>
+      <h4 class="center ">seleccione el tipo de usuario asignado </h4>
 
-</head>
-
-<body>
-
-    <div class="container">
-        <div class="row">
-
-
-
-            <div class="col l4 m4 s12 red">
-
-            </div>
-            <div class="col l4 m4 s12 center">
-                <img width="250" src="img/45.png" alt="">
-                <h3 class="center " >Iniciar Sesion</h3>
-                <h6 class="center ">seleccione el tipo de usuario asignado </h6>
-             
+      <!-- <p class="red-error">
+                    <?php
+                    session_start();
+                    if (isset($_SESSION['error'])) {
+                        echo $_SESSION['error'];
+                        unset($_SESSION['error']);
+                    }
+                    ?>
                 </p>
-                    <div class="input-field">
-                      <a href="views/loginAdmin.php"><button class="btn blue lighten-2 black-text" value="administrador black">Administrador</button></a>
+              
+
+                <p class="red-success">
+                     <?php 
+
+                    if (isset($_SESSION['vendedor'])) {
+                        echo $_SESSION['vendedor'];
+                        unset($_SESSION['vendedor']);
+                    }
+                    ?>
+                </p>
+               -->
+   
+               
+
+                <div class="input-field">
+                      <a href="views/loginAdmin.php"><button style="margin-bottom: 20px;">Administrador</button></a>
                     </div>
                     
                     <div class="input-field">
                          <a href="views/vendedor/loginVendedor.php"><button class="btn blue lighten-2 black-text" value="vendedor">Vendedor</button></a>
                     </div>
-  
-            </div>
 
-           
+                      
 
-        </div>
-
+        <a href="index.php">Home</a>
+     
     </div>
-
-
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
-
-</body>
-
+  </div>
+  </body>
 </html>
